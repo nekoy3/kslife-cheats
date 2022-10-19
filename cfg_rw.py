7435196a7a5cc5ba4df3288dfe512818be333c83
nekoy3 <nekoyama3.n@gmail.com>
2022-10-19 17:11:22 +0900

    ログイン時のパラメータを取得するためのconfigファイル読み書き用のコードを作成

---
+++ cfg_rw.py
@@ -0,0 +1,41 @@
+#https://github.com/nekoy3/raspi-nfc-botより
+import configparser
+import os
+
+config = configparser.ConfigParser()
+
+class ConfigClass:
+    pass
+
+def create_config():
+    config.read('config.ini', encoding="utf-8_sig")
+
+    config.add_section('login')
+    config.set('login', '# K\'s lifeのメールアドレスを入力')
+    config.set('login', 'mail', 'xxx@st.kyusan-u.ac.jp')
+    config.set('login', '# K\'s lifeのパスワードを入力(xxxにすることでコンソール上から入力することが出来ます。）')
+    config.set('login', 'password', 'xxx')
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+def main():
+    global config
+    try:
+        config.read('config.ini', encoding="utf-8_sig")
+
+        configClass = ConfigClass()
+        configClass.mail = str(config['login']['mail'])
+        configClass.password = str(config['login']['password'])
+
+    except Exception as e:
+        print("config.iniが存在しないか、設定が間違っています。\n" + str(e))
+        #ファイルの存在確認(カレントディレクトリにconfig.iniがあるか)
+        if not os.path.isfile('config.ini'):
+            create_config()
+        exit()
+    else:
+        return configClass
+
+if __name__ == '__main__':
+    main()
