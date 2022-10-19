95a3125dd8d87bb08ac287579415f293aaa2d748
nekoy3 <nekoyama3.n@gmail.com>
2022-10-20 08:56:11 +0900

    パスワード入力機構とbot動作用インスタンスの生成

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # coding: utf-8
 from selenium import webdriver
+from getpass import getpass
+
+import cfg_rw
+import bot
+
+configs = cfg_rw.main()
+if configs['login']['pass'] == 'xxx':
+    configs['login']['pass'] = getpass('パスワードを入力・・・: ')
+
+client = bot.MyClient(intents=mybot.intents)
 
 chrome = webdriver.Chrome(executable_path='./driver/chromedriver.exe')
 chrome.get("https://www.nttdocomo.co.jp/mydocomo/")
