70a4e152763fb2b557f95afa54522473bd61c308
nekoy3 <nekoyama3.n@gmail.com>
2022-10-23 03:08:16 +0900

    ユーザーを取得して起動メッセ―ジを送信する

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ import discord
 from discord import app_commands
 
 class MyClient(discord.Client):
-    def __init__(self):
+    def __init__(self, user_id):
         intents = discord.Intents.all()
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self) #全てのコマンドを管理するCommandTree型オブジェクトを生成
+        self.user_id = user_id
 
+    #ボットをセットアップするdiscord.Clientが持つ空のメソッド、オーバーライドすることでログイン時にon_ready()より精密に実行する
     async def setup_hook(self):
-        for id in mybot.cfg.guild_id_list:
-            guild=discord.Object(id=id)
-            self.tree.copy_global_to(guild=guild)
-            await self.tree.sync(__
+        self.channel = self.create_dm(self.get_user(self.user_id))
+        self.channel.send(content="起動しました。")
