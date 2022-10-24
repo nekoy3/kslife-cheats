249cbedba9b53e2ced456716d9c6881cd2a5db1e
nekoy3 <nekoyama3.n@gmail.com>
2022-10-24 23:03:11 +0900

    構成を変更

--- cfg_rw.py
+++ cfg_rw.py
@@ -1,39 +1,41 @@
+# coding: utf-8
 #https://github.com/nekoy3/raspi-nfc-botより
 import configparser
 import os
 
-class ConfigClass(configparser.ConfigParser()):
+class ConfigClass():
+    cfg = configparser.ConfigParser(comment_prefixes='#', allow_no_value=True)
     def create_config(self):
-        self.read('config.ini', encoding="utf-8_sig")
+        self.cfg.read('config.ini', encoding="utf-8")
 
-        self.add_section('login')
-        self.set('login', '# K\'s lifeのメールアドレスを入力')
-        self.set('login', 'mail', 'xxx@st.kyusan-u.ac.jp')
-        self.set('login', '# K\'s lifeのパスワードを入力(値をterminalにすることでコンソール上から入力することが出来る。）')
-        self.set('login', 'password', 'xxx')
+        self.cfg.add_section('login')
+        self.cfg.set('login', '# K\'s lifeのメールアドレスを入力')
+        self.cfg.set('login', 'mail', 'xxx@st.kyusan-u.ac.jp')
+        self.cfg.set('login', '# K\'s lifeのパスワードを入力(値をterminalにすることでコンソール上から入力することが出来る。）')
+        self.cfg.set('login', 'password', 'xxx')
 
-        self.add_section('discord')
-        self.set('discord', '# botのトークンを設定する')
-        self.set('discord', 'token', 'xxx')
-        self.set('discord', '# discord->ユーザ設定->マイアカウント->自分の名前の横にある「・・・」をクリック')
-        self.set('discord', '# IDをコピーすることで、ユーザIDを取得できるのでそれを設定')
-        self.set('discord', 'user_id', 0)
+        self.cfg.add_section('discord')
+        self.cfg.set('discord', '# botのトークンを設定する')
+        self.cfg.set('discord', 'token', 'xxx')
+        self.cfg.set('discord', '# discord->ユーザ設定->マイアカウント->自分の名前の横にある「・・・」をクリック')
+        self.cfg.set('discord', '# IDをコピーすることで、ユーザIDを取得できるのでそれを設定')
+        self.cfg.set('discord', 'user_id', 0)
 
         with open('config.ini', 'w') as configfile:
-            self.write(configfile)
+            self.cfg.write(configfile)
 
     def read_config(self):
         try:
-            cfg = self.read('config.ini', encoding="utf-8_sig")
+            read_cfg = self.cfg.read('config.ini')
 
-            self.mail = str(cfg['login']['mail'])
-            self.password = str(cfg['login']['password'])
-            self.token = str(cfg['discord']['token'])
-            self.user_id = int(cfg['discord']['user_id'])
+            self.mail = read_cfg['login']['mail']
+            self.password = read_cfg['login']['password']
+            self.token = read_cfg['discord']['token']
+            self.user_id = int(read_cfg['discord']['user_id'])
         
             if self.mail == 'xxx@st.kyusan-u.ac.jp' or self.password == 'xxx' or self.token == 'xxx' or self.user_id == 0:
                 raise ValueError("いずれかの値が初期値になっているので修正してください。")
-
+                
         except Exception as e:
             print("config.iniが存在しないか、設定が間違っています。\n" + str(e))
             #ファイルの存在確認(カレントディレクトリにconfig.iniがあるか)
