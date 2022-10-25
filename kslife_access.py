7e68dc058c9a8f9689a720ca904e484911ad536c
nekoy3 <nekoyama3.n@gmail.com>
2022-10-25 13:30:33 +0900

    kslife_accessの処理をクラス化
    webdriverをフィールドとして持つと便利そう

--- kslife_access.py
+++ kslife_access.py
@@ -10,8 +10,9 @@ import time
 from cfg_rw import ConfigClass
 import bot
 
-#ドライバーを準備するメソッド
-def make_driver_process() -> webdriver.Chrome:
+class BrowserClass:
+    #ドライバーを準備するメソッド
+    def make_driver_process(self) -> webdriver.Chrome:
         #ChromeOptionsクラスのインスタンスを生成して、それにオプションを追加する
         options = webdriver.ChromeOptions()
         
@@ -29,65 +30,64 @@ def make_driver_process() -> webdriver.Chrome:
 
         return chrome
 
-def sent_message(msg):
-    #bot稼働時に代わりにdiscordで送信するように実装する
-    print(msg)
+    def sent_message(self, msg):
+        #bot稼働時に代わりにdiscordで送信するように実装する
+        print(msg)
 
-def kslife_access(conigs):
-    #webdriver読み込み
-    chrome = make_driver_process()
+    def kslife_access(self, configs):
+        #webdriver読み込み
+        self.chrome = self.make_driver_process()
 
-    #K's lifeログイン前画面に移動
-    chrome.get("https://ksuweb.kyusan-u.ac.jp/portalv2/")
+        #K's lifeログイン前画面に移動
+        self.chrome.get("https://ksuweb.kyusan-u.ac.jp/portalv2/")
 
-    #ログイン画面へのボタンをクリック(XPath)
-    chrome.find_element(By.XPATH, '//*[@id="left_container"]/div/ul[1]/li/a').click()
+        #ログイン画面へのボタンをクリック(XPath)
+        self.chrome.find_element(By.XPATH, '//*[@id="left_container"]/div/ul[1]/li/a').click()
 
-    #ログイン画面をアクティブウィンドウにする
-    chrome.switch_to.window(chrome.window_handles[-1])
+        #ログイン画面をアクティブウィンドウにする
+        self.chrome.switch_to.window(self.chrome.window_handles[-1])
 
-    #ログイン画面で値を入力する
-    try:
-        chrome.find_element(By.ID, "userNameInput").send_keys(configs['login']['mail'])
-        chrome.find_element(By.ID, "passwordInput").send_keys(configs['login']['password'])
-        chrome.find_element(By.ID, "submitButton").click()
-    except: 
-        sent_message("ログイン画面に到達できませんでした。再度お試しください。")
+        #ログイン画面で値を入力する
+        try:
+            self.chrome.find_element(By.ID, "userNameInput").send_keys(configs['login']['mail'])
+            self.chrome.find_element(By.ID, "passwordInput").send_keys(configs['login']['password'])
+            self.chrome.find_element(By.ID, "submitButton").click()
+        except: 
+            self.sent_message("ログイン画面に到達できませんでした。再度お試しください。")
     
-    configs['login']['password'] = None 
+        configs['login']['password'] = None 
     
-    #サインインボタンを押した後ホーム画面に遷移するまでの処理
-    try:
-        chrome.find_element(By.ID, "errorText")
-    except: #find_elementに失敗したら正常
-        try: #K's life画面のid=homeがあれば正常遷移
-            chrome.find_element(By.ID, "home")
-        except:
-            sent_message("ログイン成功後、ホーム画面にたどり着けませんでした。メンテナンス等の原因が考えられるため、手動で確認してください。\n" + traceback.format_exc())
-            exit()
+        #サインインボタンを押した後ホーム画面に遷移するまでの処理
+        try:
+            self.chrome.find_element(By.ID, "errorText")
+        except: #find_elementに失敗したら正常
+            try: #K's life画面のid=homeがあれば正常遷移
+                self.chrome.find_element(By.ID, "home")
+            except:
+                self.sent_message("ログイン成功後、ホーム画面にたどり着けませんでした。メンテナンス等の原因が考えられるため、手動で確認してください。\n" + traceback.format_exc())
+                exit()
+            else:
+                self.sent_message("正常にホーム画面に遷移しました。")
         else:
-            sent_message("正常にホーム画面に遷移しました。")
-    else:
-        sent_message("ログインに失敗しました。IDかパスワードを間違えている可能性があります。config.iniを確認して再度お試しください。")
-        exit()
+            self.sent_message("ログインに失敗しました。IDかパスワードを間違えている可能性があります。config.iniを確認して再度お試しください。")
+            exit()
     
-    #ログイン前画面を閉じる
-    chrome.switch_to.window(chrome.window_handles[0])
-    chrome.close()
-    chrome.switch_to.window(chrome.window_handles[-1])
+        #ログイン前画面を閉じる
+        self.chrome.switch_to.window(self.chrome.window_handles[0])
+        self.chrome.close()
+        self.chrome.switch_to.window(self.chrome.window_handles[-1])
 
-    #ホーム画面からの処理
-    #出席登録画面に遷移
-    chrome.find_element(By.ID, "btnAttendance").click()
+        #ホーム画面からの処理
+        #出席登録画面に遷移
+        self.chrome.find_element(By.ID, "btnAttendance").click()
 
-    #メニューバーにカーソルを当てる
-    #actions = ActionChains(chrome)
-    #actions.move_to_element(
-    #    chrome.find_element(By.CLASS_NAME, "d_menu")
-    #).perform()
+        #メニューバーにカーソルを当てる
+        #actions = ActionChains(chrome)
+        #actions.move_to_element(
+        #    chrome.find_element(By.CLASS_NAME, "d_menu")
+        #).perform()
 
-    #授業アンケートを開く
-    #chrome.find_element(By.XPATH, "//*[@id=\"header-menu-sub\"]/li/table/tbody/tr[2]/td[1]/a[6]").click()
+        #授業アンケートを開く
+        #chrome.find_element(By.XPATH, "//*[@id=\"header-menu-sub\"]/li/table/tbody/tr[2]/td[1]/a[6]").click()
 
-    time.sleep(3)
-    main()
+        time.sleep(3)
