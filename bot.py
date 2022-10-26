b54d38f2f3fc7eddf978cc8705413da82ef9ece7
nekoy3 <nekoyama3.n@gmail.com>
2022-10-26 13:40:49 +0900

    help用embedの用意、コマンド入力処理の詳細部実装、コルーチンオブジェクトに対するawaitの追加

--- bot.py
+++ bot.py
@@ -11,13 +11,24 @@ class MyClient(discord.Client):
 
     #ボットをセットアップするdiscord.Clientが持つ空のメソッド、オーバーライドすることでログイン時にon_ready()より精密に実行する
     async def setup_hook(self):
-        self.channel = self.create_dm(self.get_user(self.user_id))
-        self.channel.send(content="起動しました。")
+        self.channel = await self.create_dm(await self.fetch_user(self.user_id))
+        await self.channel.send(content="起動しました。")
+
+    def help_embed(self) -> discord.Embed:
+        embed = discord.Embed(title="Embedのタイトル",description="Embedの概要")
+        return embed
     
     #メッセージ送信で処理するメソッド、基本的にここで処理をする
     async def on_message(self, msg):
         if msg.content.startswith('!'):
             if msg.content.startswith('!help'):
-                self.channel.send(content="helpを参照します。")
+                await self.channel.send(content="helpを参照します。", embed=self.help_embed())
+            
+            if msg.content.startswith('!stop'):
+                await self.channel.send(content="See you next again")
+                await self.close()
+
+            else:
+                await self.channel.send(content="コマンドが異なります。\"!help\"で参照してください。")
         else:
             return
