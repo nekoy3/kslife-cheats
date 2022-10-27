496c66d496c11842d2ac4fb9eac26eaa291b0c62
nekoy3 <nekoyama3.n@gmail.com>
2022-10-27 17:25:35 +0900

    コマンド処理の追加とカウント取得メソッド

--- bot.py
+++ bot.py
@@ -31,17 +31,52 @@ class MyClient(discord.Client):
         embed.add_field(name="!stop", value="botを停止します。")
         embed.set_footer(text="made by nekoy3")
         return embed
+
+    #コマンドラインを受け取って取得する個数を取得する
+    def get_count(self, args) -> int:
+        if len(args) > 2:
+            try:
+                if 1 <= int(args[2]) <= 30:
+                    return int(args[2])
+                else:
+                    return 10
+            except ValueError:
+                return 10
+        else:
+            return 10
     
     #メッセージ送信で処理するメソッド、基本的にここで処理をする
     async def on_message(self, msg):
         if msg.content.startswith('!'):
+            #ヘルプコマンドの処理
             if msg.content.startswith('!help'):
                 await self.channel.send(content="helpを参照します。", embed=self.help_embed())
             
+            #停止コマンドの処理
             elif msg.content.startswith('!stop'):
                 await self.channel.send(content="See you next again")
                 self.kslife.exit_chrome()
                 await self.close()
+            
+            #情報取得コマンドの処理
+            elif msg.content.startswith('!get'):
+                msg_args = msg.content.split(" ")
+                ermsg = "[!get] 引数\nml : 授業連絡を早い順10件取得します。\nak : 授業アンケートを早い順10件取得します。\n第二引数絵個数を1~30で取得できます。"
+                try:
+                    msg_args[1]
+                except IndexError:
+                    await self.channel.send(content=ermsg)
+                
+                #引数の値個の情報を取得する->c個
+                c = self.get_count(msg_args)
+                
+                if msg_args[1] == "ml":
+                    pass
+                elif msg_args[1] == "ak":
+                    pass
+                else:
+                    await self.channel.send(content=ermsg)
+                    return
 
             else:
                 await self.channel.send(content="コマンドが異なります。\"!help\"で参照してください。")
